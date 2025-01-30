b708a8f33ee4353cc33bbb616420d0ad9855f9cc
Keirinnnn <bryecarrein@gmail.com>
2025-01-31 01:56:39 +0800

    updated the code

--- app/views.py
+++ app/views.py
@@ -11,8 +11,10 @@ from django.contrib.auth.models import User
 class HomePageView(TemplateView):
     template_name = 'app/Home.html'
 
+
 class AboutPageView(TemplateView):
     template_name = 'app/About.html'
+
 
 class WikiDetailView(DetailView):
     model = Post
@@ -37,43 +39,49 @@ class WikiListView(ListView):
         context['total_users'] = User.objects.count()
         return context
 
+
 class WikiCreateView(CreateView):
     model = Post
     fields = ['title', 'author', 'body']
     template_name = 'app/Wiki_create.html'
-    success_url =  reverse_lazy('Add')
+    success_url = reverse_lazy('Add')
 
 class WikiUpdateView(UpdateView):
     model = Post
     fields = ['title', 'author', 'body']
     template_name = 'app/Wiki_update.html'
-    success_url =  reverse_lazy('Add')
+    success_url = reverse_lazy('Add')
+
 
 class WikiDeleteView(DeleteView):
     model = Post
     template_name = 'app/Wiki_delete.html'
-    success_url =  reverse_lazy('Add')
+    success_url = reverse_lazy('Add')
+
 
 class ContactPageView(CreateView):
     model = Contact
     form_class = ContactForm
     template_name = 'app/Contact.html'
-    success_url = reverse_lazy('Contact') 
+    success_url = reverse_lazy('Contact')
 
     def form_valid(self, form):
         messages.success(self.request, 'Your message has been sent successfully!')
         return super().form_valid(form)
+
 
 class VideoListView(ListView):
     model = Video
     template_name = 'app/video_list.html'
     context_object_name = 'videos'
 
+
 class VideoUploadView(CreateView):
     model = Video
     form_class = VideoForm
     template_name = 'app/Video.html'
     success_url = reverse_lazy('Video_list')
+
 
 class VideoDetailView(DetailView):
     model = Video
@@ -99,11 +107,13 @@ class VideoDetailView(DetailView):
             context['form'] = form
             return self.render_to_response(context)
 
+
 class ImageUploadView(CreateView):
     model = Image
     form_class = ImageUploadForm
     template_name = 'app/Image_upload.html'
     success_url = reverse_lazy('Image_gallery')
+
 
 class ImageGalleryView(ListView):
     model = Image
